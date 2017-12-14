55c05903b44946f3dee9b5837398f27c7118f7bf
Cheng-Hsi Chao <chc631@cuda2.cims.nyu.edu>
2017-12-14 17:21:03 -0500

    update

--- utils/load.py
+++ utils/load.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 from PIL import Image
 from functools import partial
-from .utils import resize_and_crop, get_square, normalize
+from .utils import resize, get_square, normalize
 
 
 def get_ids(dir):
@@ -31,12 +31,14 @@ def to_cropped_imgs(ids, dir, suffix):
 def yield_imgs(ids, dir, suffix):
     """From a list of tuples, returns the correct cropped img"""
     for id, pos in ids:
+        # import pdb; pdb.set_trace()
         # im = resize_and_crop(Image.open(dir + id + suffix))
         im = Image.open(dir + id + suffix)
+        im = resize(im, 0.1)
         # yield get_square(im, pos)
         yield im
 
-def yield_masks(ids, dir, suffix):
+def yield_depth_masks(ids, dir, suffix):
     """From a list of tuples, returns the correct cropped img"""
     for id, pos in ids:
         # im = resize_and_crop(Image.open(dir + id + suffix))
@@ -44,20 +46,34 @@ def yield_masks(ids, dir, suffix):
         yield df.values.tolist()
         # yield pd.Series(df.T.to_dict('list'))
 
+def yield_masks(ids, dir, suffix):
+    """From a list of tuples, returns the correct cropped img"""
+    # import pdb; pdb.set_trace()
+    for id, pos in ids:
+        im = Image.open(dir + id + suffix)
+        im = resize(im, 0.1)
+        # yield get_square(im, pos)
+        yield im
+
 
 def get_imgs_and_masks(ids, dir_img, dir_mask):
     """Return all the couples (img, mask)"""
 
     # imgs = to_cropped_imgs(ids, dir_img, '.jpg')
+    # import pdb; pdb.set_trace()
 
     # need to transform from HWC to CHW
     imgs = yield_imgs(ids, dir_img, ".jpg") 
+    # import pdb; pdb.set_trace()
     imgs_switched = map(partial(np.transpose, axes=[2, 0, 1]), imgs)
-    imgs_normalized = map(normalize, imgs_switched)
+    # imgs_normalized = map(normalize, imgs_switched)
+ 
+    masks = yield_masks(ids, dir_mask, '.jpg')
+    masks_switched = map(partial(np.transpose, axes=[2, 0, 1]), imgs)
+    # masks_normalized = map(normalize, imgs_switched)
 
-    masks = yield_masks(ids, dir_mask, '.txt')
-
-    return zip(imgs_normalized, masks)
+    # return zip(imgs_normalized, masks_normalized)
+    return zip(imgs_switched, masks_switched)
 
 def get_full_img_and_mask(id, dir_img, dir_mask):
     im = Image.open(dir_img + id + '.jpg')
