abe3e089d80c1087a276bad91386215a32268526
hushenghao <hushenghao@baidu.com>
2020-04-17 10:36:05 +0800

    optimize the passed in parameters in the case of bilinear

--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -48,7 +48,7 @@ class Up(nn.Module):
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels // 2, in_channels // 2)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
         else:
             self.up = nn.ConvTranspose2d(in_channels , in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
