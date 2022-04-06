6cd4b68c997542cf0a9ca205120fd4f275e3cb24
Gouvernathor <aanonyme.personne@hotmail.fr>
2022-04-06 13:45:19 +0200

    change the order of asserts

--- utils/data_loading.py
+++ utils/data_loading.py
@@ -58,8 +58,8 @@ class BasicDataset(Dataset):
         mask_file = list(self.masks_dir.glob(name + self.mask_suffix + '.*'))
         img_file = list(self.images_dir.glob(name + '.*'))
 
-        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
         assert len(img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
+        assert len(mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
         mask = self.load(mask_file[0])
         img = self.load(img_file[0])
 
