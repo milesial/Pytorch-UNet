1ce4f16b08f85f46b4fcbf28f13e05bef035c829
Labib Shahid <lshahid@DMP-WK-DLGP903.medphysics>
2023-10-25 15:55:27 -0500

    Convert jpg in imgs/ dir to binary gif in masks/

--- bladder_data/mask_gif_generator.py
+++ bladder_data/mask_gif_generator.py
@@ -1,23 +1,38 @@
+import os
 from PIL import Image
 import numpy
 import matplotlib.pyplot as plt
 import imageio
- 
-# Import image as grayscale array
-img= Image.open("BPH009_mask+000056-000.jpg").convert('L')
-gray_arr = numpy.array(img)
 
-# Convert to binary array
-thresh_min = 50
-thresh_max = 70
-bin_arr = (gray_arr >= thresh_min) & (gray_arr <= thresh_max)
-bin_arr = bin_arr.astype('uint8')*255
-bin_arr[200:, :] = 0
+# Image directory
+img_dir = 'imgs_wmasks'
+gif_dir = 'masks\\'
 
-# Plot
-plt.imshow(bin_arr, cmap='gray', vmin=0, vmax=255)
-plt.axis('off')
-plt.show()
+# Iterate over files in image directory
+for img_name in os.listdir(img_dir):
+    jpg_name = os.path.join(img_dir, img_name)
 
-# Save as gif
-imageio.imsave("mask.gif", bin_arr)
+    # Import image as grayscale array
+    img = Image.open(jpg_name).convert('L')
+    gray_arr = numpy.array(img)
+    
+    # # Plot gray
+    # plt.imshow(gray_arr, cmap='gray', vmin=0, vmax=255)
+    # plt.axis('off')
+    # plt.show()
+    
+    # Convert to binary array
+    thresh_min = 50
+    thresh_max = 250
+    bin_arr = (gray_arr >= thresh_min) & (gray_arr <= thresh_max)
+    bin_arr[200:, :] = 0
+    bin_arr = bin_arr.astype('uint8')*255
+    
+    # # Plot binary
+    # plt.imshow(bin_arr, cmap='gray', vmin=0, vmax=255)
+    # plt.axis('off')
+    # plt.show()
+    
+    # Save as gif
+    gif_name = gif_dir + img_name[:16] + img_name[19:23] + '.gif'
+    imageio.imsave(gif_name, bin_arr)
