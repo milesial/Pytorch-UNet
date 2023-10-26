2548caab9eec3d965b9edeb4b6b57a5ffb2a0772
Labib Shahid <lshahid@wisc.edu>
2023-10-26 00:31:02 +0000

    test1 runs, poor dice score

--- train.py
+++ train.py
@@ -193,7 +193,7 @@ if __name__ == '__main__':
     # Change here to adapt to your data
     # n_channels=3 for RGB images
     # n_classes is the number of probabilities you want to get per pixel
-    model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
+    model = UNet(n_channels=1, n_classes=args.classes, bilinear=args.bilinear)
     model = model.to(memory_format=torch.channels_last)
 
     logging.info(f'Network:\n'
