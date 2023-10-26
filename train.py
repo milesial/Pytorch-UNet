500c2c7f51d77cc9fc1bfc19b78b5c0d6d958aa9
Labib Shahid <lshahid@wisc.edu>
2023-10-26 00:15:45 +0000

    Finished testing edits on medphysics machine and container

--- train.py
+++ train.py
@@ -1,6 +1,3 @@
-# Typed inside container
-# Typed on medphysics machine
-
 import argparse
 import logging
 import os
@@ -196,7 +193,7 @@ if __name__ == '__main__':
     # Change here to adapt to your data
     # n_channels=3 for RGB images
     # n_classes is the number of probabilities you want to get per pixel
-    model = UNet(n_channels=1, n_classes=args.classes, bilinear=args.bilinear)
+    model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
     model = model.to(memory_format=torch.channels_last)
 
     logging.info(f'Network:\n'
